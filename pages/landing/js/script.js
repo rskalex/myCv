599391c0de9b4de7c5dceae9b339ee70c9ab4475
Alex <dobrovolya48@gmail.com>
2022-02-15 13:53:34 +0300

    fix: fix index.js

--- pages/landing/js/script.js
+++ pages/landing/js/script.js
@@ -1,18 +1,5 @@
-const headerLinkBtn = document.querySelectorAll('.menu-link');
-const body = document.querySelector('.body');
-const menuIcon = document.querySelector('.menu-icon');
-const menuBody = document.querySelector('.menu-body');
+function explode(s) {
+  console.log(s.split("").map((e) => e.repeat(e)).join(""));
+}
 
-menuIcon.addEventListener('click', function(elem) {
-    menuIcon.classList.toggle('_active');
-    menuBody.classList.toggle('_active');
-    body.classList.toggle('lock');
-});
-
-
-headerLinkBtn.forEach(item => {
-  item.addEventListener('click', (e) => {
-    headerLinkBtn.forEach(link => link.classList.remove('active'));
-    e.target.classList.add('active');
-  });
-})
+explode('32')
